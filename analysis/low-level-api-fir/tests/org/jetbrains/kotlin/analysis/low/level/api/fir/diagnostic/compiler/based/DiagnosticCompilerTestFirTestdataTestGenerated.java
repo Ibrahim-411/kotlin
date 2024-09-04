eaa5720094dc5e06c0c964a601a4a39fc38580f9
Leonid Startsev <leonid.startsev@jetbrains.com>
2024-09-04 20:14:53 +0200

    exclude lists

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/DiagnosticCompilerTestFirTestdataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/DiagnosticCompilerTestFirTestdataTestGenerated.java
@@ -1840,6 +1840,12 @@ public class DiagnosticCompilerTestFirTestdataTestGenerated extends AbstractDiag
       }
 
       @Test
+      @TestMetadata("exclusions.kt")
+      public void testExclusions() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/crv/exclusions.kt");
+      }
+
+      @Test
       @TestMetadata("lambdas.kt")
       public void testLambdas() {
         runTest("compiler/fir/analysis-tests/testData/resolve/crv/lambdas.kt");
