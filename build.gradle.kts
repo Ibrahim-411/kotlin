edac686d2f759296445cb2944c6aa1144a7f5873
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-10-28 12:47:09 +0100

    Move litmuskt tests into native/native.tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -826,7 +826,7 @@ tasks {
         dependsOn(":native:objcexport-header-generator:check")
         dependsOn(":native:swift:swift-export-standalone:test")
         dependsOn(":native:swift:swift-export-ide:test")
-        dependsOn(":litmuskt:repo-tests:check")
+        dependsOn(":native:native.tests:litmus-tests:check")
     }
 
     // These are unit tests of Native compiler
