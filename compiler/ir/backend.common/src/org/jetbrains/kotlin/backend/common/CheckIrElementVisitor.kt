97e60c322ce968357c6050279167d1455dd0de84
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-11-01 22:21:16 +0200

    fixup! [IR][K/N] Swapped autoboxing & generic calls return type eraser

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.ir.symbols.IrSymbol
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.isNullable
 import org.jetbrains.kotlin.ir.util.render
-import org.jetbrains.kotlin.ir.util.target
+import org.jetbrains.kotlin.ir.util.resolveFakeOverrideMaybeAbstract
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 
 typealias ReportError = (element: IrElement, message: String) -> Unit
@@ -167,7 +167,7 @@ internal class CheckIrElementVisitor(
 
         // TODO: Why don't we check parameters as well?
 
-        val returnType = expression.symbol.owner.target.returnType
+        val returnType = expression.symbol.owner.let { it.resolveFakeOverrideMaybeAbstract() ?: it }.returnType
         // TODO: We don't have the proper type substitution yet, so skip generics for now.
         if (returnType is IrSimpleType &&
             returnType.classifier is IrClassSymbol &&
