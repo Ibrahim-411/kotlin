8f78f4b1a4f487960d6fb103a6d41749ef9ebb64
Oleg Makeev <oleg.makeev@jetbrains.com>
2024-11-11 16:13:09 +0100

    [Analysis API] Add `KaValueParameterType` and `KaFirFunctionType.parameters`
    Before that change, there was no convenient API to retrieve
    parameters' names from `KaFirFunctionType`. The only place where
    these names are stored is attributes of parameters' types.
    Simple `KaValueParameterType` is added to flawlessly provide
    pairs of Name-Type to users.
    
    ^KT-71876 fixed

---
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/resolution/KaBaseReceiverFunctionValueParameter.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.kotlin.analysis.api.impl.base.resolution
+
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
+import org.jetbrains.kotlin.analysis.api.types.KaReceiverFunctionValueParameter
+import org.jetbrains.kotlin.analysis.api.types.KaType
+
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@KaExperimentalApi
+class KaBaseReceiverFunctionValueParameter(override val type: KaType) : KaReceiverFunctionValueParameter
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/stages/CheckCallableReferenceExpectedType.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/stages/CheckCallableReferenceExpectedType.kt
@@ -11,6 +11,7 @@ import org.jetbrains.kotlin.builtins.functions.FunctionTypeKind
 import org.jetbrains.kotlin.builtins.functions.isBasicFunctionOrKFunction
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.fir.*
+import org.jetbrains.kotlin.fir.builder.FirFunctionTypeParameterBuilder
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.expressions.*
 import org.jetbrains.kotlin.fir.expressions.builder.buildNamedArgumentExpression
@@ -142,7 +143,14 @@ private fun buildResultingTypeAndAdaptation(
             val returnTypeWithoutCoercion = returnTypeRef.coneType
             val returnType = if (callableReferenceAdaptation == null) {
                 returnTypeWithoutCoercion.also {
-                    fir.valueParameters.mapTo(parameters) { it.returnTypeRef.coneType }
+                    fir.valueParameters.mapTo(parameters) { valueParameter ->
+                        val typeParameterBuilder = FirFunctionTypeParameterBuilder().apply {
+                            this.name = valueParameter.name
+                            this.source = valueParameter.source
+                            this.returnTypeRef = valueParameter.returnTypeRef
+                        }
+                        valueParameter.returnTypeRef.coneType.withParameterNameAnnotation(typeParameterBuilder.build())
+                    }
                 }
             } else {
                 parameters += callableReferenceAdaptation.argumentTypes
