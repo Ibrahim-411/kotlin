6755d69d0400aba4ec0706408f078a56e83cccda
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-11-13 16:15:40 +0100

    [IR] Migrate backend.jvm to new IR parameter API A29

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmSymbols.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmSymbols.kt
@@ -1047,13 +1047,15 @@ class JvmSymbols(
         }
 
     private val defaultValueOfFunction = javaLangString.functions.single {
-        it.owner.name.asString() == "valueOf" && it.owner.valueParameters.singleOrNull()?.type?.isNullableAny() == true
+        it.owner.name.asString() == "valueOf"
+                && it.owner.hasShape(dispatchReceiver = true, regularParameters = 1, parameterTypes = listOf(null, irBuiltIns.anyNType))
     }
 
     private val valueOfFunctions: Map<IrType, IrSimpleFunctionSymbol?> =
         context.irBuiltIns.primitiveIrTypes.associateWith { type ->
             javaLangString.functions.singleOrNull {
-                it.owner.name.asString() == "valueOf" && it.owner.valueParameters.singleOrNull()?.type == type
+                it.owner.name.asString() == "valueOf"
+                        && it.owner.hasShape(dispatchReceiver = true, regularParameters = 1, parameterTypes = listOf(null, type))
             }
         }
 
